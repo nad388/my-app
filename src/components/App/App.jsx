36f12547a7f6c6203e3b26018abf211d7e060572
Nadezhda Dmitrieva <n.dmitrieva388@gmail.com>
2025-08-19 20:38:24 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,16 @@
+import MyName from '../MyName/MyName'
+import './App.css'
+
+function App() {
+	return (
+		<>
+			<div>
+				<h1>Hello! I am a Doppelganger!</h1>
+				<p>Who killed Laura Palmer?</p>
+				<MyName />
+			</div>
+		</>
+	)
+}
+
+export default App
---
+++ src/components/MyName/MyName.jsx
@@ -0,0 +1,7 @@
+import './MyName.css'
+
+function MyName() {
+	return <h2>My name is Nadezhda.</h2>
+}
+
+export default MyName
